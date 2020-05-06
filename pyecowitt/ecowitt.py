9438a22029cabe1b94ef94cf81ffb33e27203737
garbled1 <root@garbled.net>
2020-05-06 16:20:03 -0700

    hacks?

--- pyecowitt/ecowitt.py
+++ pyecowitt/ecowitt.py
@@ -2,6 +2,7 @@ import asyncio
 from aiohttp import web
 import logging
 import math
+import time
 
 """
 Setup a bone simple webserver, listen to POST's from a device like a
@@ -21,7 +22,10 @@ class EcoWittListener:
         self.runner = None
         self.site = None
         self.r_listeners = []
+        self.last_values = {}
+        self.data_valid = False
         self.log = logging.getLogger(__name__)
+        self.lastupd = 0
 
     def register_listener(self, function):
         self.r_listeners.append(function)
@@ -146,10 +150,14 @@ class EcoWittListener:
     async def handler(self, request: web.BaseRequest):
         if (request.method == 'POST'):
             data = await request.post()
+            # data is not a dict, it's a MultiDict
             data_copy = {}
             for k in data.keys():
                 data_copy[k] = data[k]
             weather_data = self.convert_units(data_copy)
+            self.last_values = weather_data.copy()
+            self.data_valid = True
+            self.lastupd = time.time()
             for rl in self.r_listeners:
                 try:
                     await rl(weather_data)
@@ -157,6 +165,12 @@ class EcoWittListener:
                     pass
 
         return web.Response(text="OK")
+
+    async def wait_for_valid_data(self):
+        """ Wait for valid data, then return true. """
+        while not self.data_valid:
+            await asyncio.sleep(1)
+        return self.data_valid
 
     async def listen(self):
         """ Listen and process."""
@@ -170,8 +184,10 @@ class EcoWittListener:
         while True:
             await asyncio.sleep(10000)
 
+    async def stop(self):
+        await self.site.stop()
+
     async def start(self):
-        print("BOOO")
         loop = asyncio.get_event_loop()
         try:
             task = loop.create_task(self.listen())
